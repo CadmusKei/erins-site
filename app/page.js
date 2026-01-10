1837f6064f3a14f19923d957742eb74d820a686d
Kei <maxwell.kei.farouk@gmail.com>
2026-01-10 13:46:34 +0200

    Add contact information section to Home component

--- app/page.js
+++ app/page.js
@@ -56,6 +56,12 @@ export default function Home() {
           </p>
         </div>
         <Divider />
+        <Title size="text-6xl">Contact</Title>
+        <div className="w-[75%] mt-3 flex flex-col space-y-5 items-center">
+          <p>Email: erinsalo6@gmail.com</p>
+          <p>Phone Number: 079 540 8160</p>
+        </div>
+        <Divider />
       </div>
     </div>
   );
