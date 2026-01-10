31dcacbe98717782b74d6f588566413e7a81f7db
Kei <maxwell.kei.farouk@gmail.com>
2026-01-10 11:47:09 +0200

    initial app creation

---
+++ app/components/Title.js
@@ -0,0 +1,10 @@
+import { Imperial_Script } from "next/font/google";
+
+const imperialScript = Imperial_Script({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export default function Title({children}){
+    return(<h1 className={`${imperialScript.className} text-7xl text-shadow-lg text-shadow-black`}>{children}</h1>)
+}
